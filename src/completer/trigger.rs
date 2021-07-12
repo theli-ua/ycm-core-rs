b5f1e1b09a15caf356d80830976525b96ceec093
Anton Romanov <theli.ua@gmail.com>
2021-07-11 23:27:32 -0700

    Add trigger matcher

--- src/completer/trigger.rs
+++ src/completer/trigger.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
-use regex::{escape, RegexSet};
+use regex::{escape, Regex, RegexSet};
 
 const REGEX_PREFIX: &str = "re!";
 
@@ -35,12 +35,59 @@ pub fn parse_triggers(
     res
 }
 
+pub trait PatternMatcher {
+    fn matches_for_filetype(
+        &self,
+        filetype: &str,
+        line: &str,
+        start_codepoint: usize,
+        column_codepoint: usize,
+    ) -> bool;
+}
+
+impl PatternMatcher for HashMap<String, RegexSet> {
+    fn matches_for_filetype(
+        &self,
+        filetype: &str,
+        line: &str,
+        start_codepoint: usize,
+        column_codepoint: usize,
+    ) -> bool {
+        let line = if column_codepoint < line.len() {
+            &line[..column_codepoint]
+        } else {
+            &line[..]
+        };
+        match self.get(filetype) {
+            None => false,
+            Some(re) => {
+                for m in re.matches(line) {
+                    for m in Regex::new(&re.patterns()[m]).unwrap().find_iter(line) {
+                        /*
+                            By definition of 'start_codepoint', we know that the character just before
+                            'start_codepoint' is not an identifier character but all characters
+                            between 'start_codepoint' and 'column_codepoint' are. This means that if
+                            our trigger ends with an identifier character, its tail must match between
+                            'start_codepoint' and 'column_codepoint', 'start_codepoint' excluded. But
+                            if it doesn't, its tail must match exactly at 'start_codepoint'. Both
+                            cases are mutually exclusive hence the following condition.
+                        */
+                        if start_codepoint <= m.end() && m.end() <= column_codepoint {
+                            return true;
+                        }
+                    }
+                }
+                false
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_parse_triggers() {
-        let input = vec![
+    fn get_default() -> HashMap<String, Vec<String>> {
+        vec![
             ("c".into(), vec![".".into(), "->".into()]),
             (
                 "objc,objcpp".into(),
@@ -49,7 +96,11 @@ mod tests {
             ("objc".into(), vec!["foo".into()]),
         ]
         .into_iter()
-        .collect::<HashMap<String, Vec<String>>>();
+        .collect::<HashMap<String, Vec<String>>>()
+    }
+    #[test]
+    fn test_triggers() {
+        let input = get_default();
 
         let output = parse_triggers(vec![input], &HashSet::default());
 
@@ -67,5 +118,12 @@ mod tests {
 
         assert!(output["objc"].is_match("foo"));
         assert!(!output["objcpp"].is_match("foo"));
+    }
+
+    #[test]
+    fn test_matcher() {
+        let triggers = parse_triggers(vec![get_default()], &HashSet::default());
+        assert!(triggers.matches_for_filetype("c", "foo->bar", 5, 9));
+        assert!(!triggers.matches_for_filetype("c", "foo::bar", 5, 9));
     }
 }
